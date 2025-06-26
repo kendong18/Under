cd5aedabf3d98ad73983222e8ea1a07c0c66ea8d
Geun <33642342+kendong18@users.noreply.github.com>
2025-06-26 19:33:18 +0900

    오브젝트와 광원
    오브젝트 이동 및 카메라 고정, 광원 설정

--- Assets/Scripts/CubeRotator.cs
+++ Assets/Scripts/CubeRotator.cs
@@ -10,16 +10,26 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         
     }
-
-    // Update is called once per frame
+    public float moveSpeed = 5f;
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Rotate(0, 50*Time.deltaTime, 0);
-        }else if (Input.GetKey(KeyCode.KeypadEnter))
-        {
-            transform.Rotate(0, 50 * Time.deltaTime, 0);
-        }
+        // if (Input.GetKey(KeyCode.Space))
+        // {
+        //     transform.Rotate(0, 50 * Time.deltaTime, 0);
+        // }
+        // else if (Input.GetKey(KeyCode.KeypadEnter))
+        // {
+        //     transform.Rotate(0, 50 * Time.deltaTime, 0);
+        // }
+
+        // 1. 사용자 입력받기 
+        float horizontalInput = Input.GetAxis("Horizontal"); // 좌우 입력
+        float verticalInput = Input.GetAxis("Vertical"); // 상하 입력
+
+        // 2. 이동 방향 계산하기 
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        // 3. 오브젝트 이동시키기
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
     }
 }
